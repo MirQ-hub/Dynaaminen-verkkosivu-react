d4b71c4e5aef1e53d7ef814505ef4dac3c0391e8
MirQ-hub <n2vami00@students.oamk.fi>
2023-02-16 09:41:45 +0200

    valuutta

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [eurot, setEurot] = useState(0)
+  const [punnat, setPunnat] = useState(0)
+  
+function laske(e){
+e.preventDefault()
+setPunnat(eurot*0.9)
+}
+
   return (
      <form onSubmit={laske}>
    <h3>Valuuttalaskuri</h3>
    <div>
    <label>Eurot</label>
-   <input value={eurot} onChange={e => setEurot(e.target.
+   <input value={eurot} onChange={e =>setEurot(e.target.
 value)} type='number'/>
    </div>
    <div>
